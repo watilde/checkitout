a80931b7f279a8e121765e2353405ca88e2a94ea
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-11-13 11:54:18 +0900

    Add a feature to do git clone with reference

--- init.js
+++ init.js
@@ -1,9 +1,13 @@
 var request = require('request');
+var async = require('async');
 var _ = require('underscore');
+var exec = require('child_process').exec;
+/* @custom area */
 var org = 'watilde';
 var repo = 'css-filter.js'
+var default_branch = 'master';
+
 var url = 'https://api.github.com/repos/' + org + '/' + repo + '/branches';
-var exec = require('child_process').exec;
 
 var options = {
   url: url,
@@ -15,17 +19,34 @@ var options = {
 request(options, function (error, response, body) {
   var branches = JSON.parse(body);
   if (!error && response.statusCode == 200) {
-    _.each(branches, function (branch) {
+    async.waterfall(function () {
       var name = branch.name;
       var clone_url = 'git@github.com:' + org + '/' + repo + '.git';
-      exec('git clone -b ' + name + ' ' + clone_url + ' branches/' + name,
+      exec('git clone --recursive -b ' + name + ' ' + clone_url + ' branches/' + default_branch,
         function (error, stdout, stderr) {
           console.log('stdout: ' + stdout);
           console.log('stderr: ' + stderr);
           if (error !== null) {
             console.log('exec error: ' + error);
-          }          
-      });
-    });
+          } else {
+            delete branches.master;
+            callback();
+          }
+        }
+      );
+    }, async.each(branches, function (branch, callback) {
+      var name = branch.name;
+      var clone_url = 'git@github.com:' + org + '/' + repo + '.git';
+      exec('git clone --recursive --reference branches/' + default_branch + ' -b ' + name + ' ' + clone_url + ' branches/' + name,
+        function (error, stdout, stderr) {
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+          callback();
+        }
+      );
+    }));
   }
 });
