bba87cef5426ff910ce52e66a8c95d0b490a55da
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-11-14 17:16:13 +0900

    Fixes get index file

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var http = require('http');
 var serveIndex = require('serve-index');
 var serveStatic = require('serve-static');
 
-var index = serveIndex(__dirname, {'icons': true});
-var serve = serveStatic('/');
+var index = serveIndex('repos', {'icons': true});
+var serve = serveStatic('repos');
 
 // Create server
 var server = http.createServer(function onRequest(req, res){
-  var done = finalhandler(req, res)
-  serve(req, res, function onNext(err) {
+  var done = finalhandler(req, res);
+  serve(req, res, function (err) {
     if (err) return done(err)
     index(req, res, done);
-  })
+  });
 })
 
 // Listen
