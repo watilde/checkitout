f9ceec6ee43b12288d5a0f66c6e8d16c965caa2a
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-11-14 16:39:03 +0900

    Fixes path names

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveIndex = require('serve-index')
-var serveStatic = require('serve-static')
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveIndex = require('serve-index');
+var serveStatic = require('serve-static');
 
-var index = serveIndex('repos/', {'icons': true})
-var serve = serveStatic('repos/')
+var index = serveIndex(__dirname, {'icons': true});
+var serve = serveStatic('/');
 
 // Create server
 var server = http.createServer(function onRequest(req, res){
   var done = finalhandler(req, res)
   serve(req, res, function onNext(err) {
     if (err) return done(err)
-    index(req, res, done)
+    index(req, res, done);
   })
 })
 
