3119063e62b9e978f1b7273a83e3ab38ce75be3e
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-11-14 19:55:15 +0900

    Add tmp index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-var finalhandler = require('finalhandler');
-var serveIndex = require('serve-index');
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var connectInclude = require('connect-include');
+var express = require('express');
+var _serveIndex = require('serve-index');
+var _serveStatic = require('serve-static');
+var fs = require('fs');
 
-var index = serveIndex('./', {'icons': true});
-var serve = serveStatic('./');
-var include = connectInclude('./');
+var serveIndex = _serveIndex('./', {'icons': true});
+var serveStatic = _serveStatic('./');
 
-var app = connect();
+
+var app = express();
 
 // Create server
-app.use(function (req, res) {
-  var done = finalhandler(req, res);
-  include(req, res ,function (err) {
-    if (err) return done(err);
-    serve(req, res, function (err) {
-      if (err) return done(err);
-      index(req, res, done);
-    });
-  });
+app.use(function (req, res, next) {
+  var shtml = (req.path.split('.').pop() === 'shtml');
+  if (!shtml) return next();
+  var data = fs.readFileSync('.' + req.path);
+  console.log(data);
+  next();
 });
+
+app.use(serveStatic);
+app.use(serveIndex);
 
 // Listen
 app.listen(3000);
