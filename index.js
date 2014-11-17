2606c20b5434211576ab12f77eaf29aaa60b0c3e
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-11-17 09:34:39 +0900

    Supports shtml file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var _serveIndex = require('serve-index');
 var _serveStatic = require('serve-static');
 var fs = require('fs');
+var ssi = require('ssi');
 
 var serveIndex = _serveIndex('./', {'icons': true});
 var serveStatic = _serveStatic('./');
@@ -9,13 +10,22 @@ var serveStatic = _serveStatic('./');
 
 var app = express();
 
-// Create server
+// Support Server Side Includes
 app.use(function (req, res, next) {
-  var shtml = (req.path.split('.').pop() === 'shtml');
-  if (!shtml) return next();
-  var data = fs.readFileSync('.' + req.path);
-  console.log(data);
-  next();
+  var filename = '.' + req.path;
+  var isShtml = (filename.split('.').pop() === 'shtml');
+  if (fs.existsSync(filename) === false) return next();
+  if (isShtml === false) return next();
+
+  var parser = new ssi(__dirname, '', '');
+  var data = fs.readFileSync(filename, {encoding: 'utf8'});
+  var content = parser.parse(filename, data).contents;
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': Buffer.byteLength(content, 'utf8')
+  });
+  res.end(content);
 });
 
 app.use(serveStatic);
