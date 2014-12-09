30c6634de958acee98ded4f915d9121860319a1b
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-12-09 11:14:32 +0900

    Add pulljs

---
+++ pull.js
@@ -0,0 +1,8 @@
+var async = require('async');
+var request = require('request');
+var rimraf = require('rimraf');
+var _ = require('underscore');
+var exec = require('child_process').exec;
+var checkitout = require('./checkitout.json');
+
+async.waterfall();
